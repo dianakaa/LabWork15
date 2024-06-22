badf603f6ac5b5c06ec2ffac4c623152c4e8963d
ki1lmyself <sofiamiklzkova@gmail.com>
2024-06-22 17:47:27 +0300

    Добавьте файлы проекта.

---
+++ LabWork15/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace LabWork15
+{
+    delegate int Operation(int x);
+
+    internal class Program
+    {
+        static int Square(int x)
+        {
+            return x * x;
+        }
+
+        static int Factorial(int x)
+        {
+            int a = 1;
+            if (x == 0) 
+                return 1;
+            for (int i = 1; i <= x; i++)
+            {
+                a *= i;
+            }
+            return a;
+        }
+
+        static int Absolute(int x)
+        {
+            if (x<0)
+                return -x;
+            return x;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = -5;
+
+            Operation Operation = Square;
+            Console.WriteLine("Квадрат: {1}", number, Operation(number));
+
+            Operation = Factorial;
+            Console.WriteLine("Факториал: {1}", number, Operation(number));
+
+            Operation = Absolute;
+            Console.WriteLine("Модуль: {1}", number, Operation(number));
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_2
+{
+    delegate void Operation(int a, int b);
+    internal class Program
+    {
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine($"Сумма {a + b}");
+        }
+
+        static void Difference(int a, int b)
+        {
+            Console.WriteLine($"Разность {a - b}");
+        }
+
+        static void Multiplication(int a, int b)
+        {
+            Console.WriteLine($"Произведение {a * b}");
+        }
+
+        static void Division(int a, int b)
+        {
+            if (b != 0)
+                Console.WriteLine($"Частное {a / b}");
+            else
+                Console.WriteLine("На 0 делить нельзя");
+        }
+
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation(Sum);
+            operation += Difference;
+            operation += Multiplication;
+            operation += Division;
+            operation(10, 0);
+        }
+    }
+}
